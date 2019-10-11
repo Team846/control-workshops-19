403c3e6cc507a3d6d0269dde366487920e87be34
Andy Min <andrewkmin@gmail.com>
2019-10-10 19:45:53 -0700

    Add solenoid/trigger template

--- src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
+++ src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
@@ -2,8 +2,9 @@ package com.lynbrookrobotics.workshops
 
 import edu.wpi.first.hal.HAL
 import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.Solenoid
+import edu.wpi.first.wpilibj.Joystick
 
-// BASE TEMPLATE
 fun main(args: Array<String>) = RobotBase.startRobot(::FunkyRobot)
 
 class FunkyRobot : RobotBase() {
@@ -12,11 +13,19 @@ class FunkyRobot : RobotBase() {
 
         // write initialization code here!
 
+        val pivot = /* Make a solenoid! */
+        val joystick = /* Make a joystick */
+
         HAL.observeUserProgramStarting()
 
         while (true) {
 
             // write control code here!
+
+            if (/* Check if the joystick trigger is pressed */) {
+                /* Move the pivot down! */
+            }
+
 
             m_ds.waitForData()
         }
