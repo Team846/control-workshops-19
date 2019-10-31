12c062b1b41d5ee9d61efd92150bfafb708ce90b
Andy Min <andrewkmin@gmail.com>
2019-10-30 23:33:54 -0700

    Add lift code

--- src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
+++ src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
@@ -1,7 +1,10 @@
 package com.lynbrookrobotics.workshops
 
 import edu.wpi.first.hal.HAL
+import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.Solenoid
+import info.kunalsheth.units.generated.Percent
 
 // BASE TEMPLATE
 fun main(args: Array<String>) = RobotBase.startRobot(::FunkyRobot)
@@ -10,13 +13,29 @@ class FunkyRobot : RobotBase() {
 
     override fun startCompetition() {
 
-        // write initialization code here!
+        val hook = Solenoid(2)
+        val lift = Lift()
+        val joystick = Joystick(0)
+
+        var counter = 0
 
         HAL.observeUserProgramStarting()
 
         while (true) {
 
-            // write control code here!
+            hook.set(joystick.getRawButton(1))
+            lift.set(
+                    when (joystick.pov) {
+                        0 -> 50.Percent
+                        180 -> -50.Percent
+                        else -> 0.Percent
+                    }
+            )
+
+            counter = (counter + 1) % 10
+            if (counter == 0) {
+                print(lift.position)
+            }
 
             m_ds.waitForData()
         }
---
+++ src/main/kotlin/com/lynbrookrobotics/workshops/Lift.kt
@@ -0,0 +1,87 @@
+package com.lynbrookrobotics.workshops
+
+import com.ctre.phoenix.motorcontrol.ControlFrame.Control_3_General
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.NeutralMode
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod.Period_5Ms
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration
+import info.kunalsheth.units.generated.*
+import info.kunalsheth.units.math.milli
+
+class Lift {
+
+    companion object {
+        private const val ESC_CAN_ID = 40
+        private const val IDX = 0
+        private const val INVERT = false
+        private const val INVERT_SENSOR = false
+
+        private const val TIMEOUT = 2500
+
+        private val syncThreshold = 5.milli(Second)
+        private val openloopRamp = 0.Second
+        private val closedloopRamp = 0.Second
+        private val peakOutputForward = 12.Volt
+        private val nominalOutputForward = 1.Volt
+        private val nominalOutputReverse = -nominalOutputForward
+        private val peakOutputReverse = -peakOutputForward
+        private val voltageCompSaturation = 12.Volt
+        private val continuousCurrentLimit = 20.Ampere
+        private val peakCurrentLimit = 35.Ampere
+        private val peakCurrentDuration = 0.5.Second
+
+        private val min = 0.Inch to 156
+        private val max = 67.Inch to 756
+        private val zeroOffset = 17.42.Inch
+    }
+
+    private val talon = TalonSRX(ESC_CAN_ID).apply {
+        configFactoryDefault(TIMEOUT)
+        setNeutralMode(NeutralMode.Brake)
+        configNeutralDeadband(0.001, TIMEOUT)
+        enableVoltageCompensation(true)
+        enableCurrentLimit(true)
+        setControlFramePeriod(Control_3_General, syncThreshold.milli(Second).toInt())
+        inverted = INVERT
+        setSensorPhase(INVERT_SENSOR)
+
+        configForwardSoftLimitEnable(true, TIMEOUT)
+        configReverseSoftLimitEnable(true, TIMEOUT)
+        configForwardSoftLimitThreshold(max.second, TIMEOUT)
+        configReverseSoftLimitThreshold(min.second, TIMEOUT)
+
+        configOpenloopRamp(openloopRamp.Second, TIMEOUT)
+        configClosedloopRamp(closedloopRamp.Second, TIMEOUT)
+        configPeakOutputForward((peakOutputForward / voltageCompSaturation).Each, TIMEOUT)
+        configPeakOutputReverse((peakOutputReverse / voltageCompSaturation).Each, TIMEOUT)
+        configNominalOutputForward((nominalOutputForward / voltageCompSaturation).Each, TIMEOUT)
+        configNominalOutputReverse((nominalOutputReverse / voltageCompSaturation).Each, TIMEOUT)
+        configVoltageCompSaturation(voltageCompSaturation.Volt, TIMEOUT)
+        configContinuousCurrentLimit(continuousCurrentLimit.Ampere.toInt(), TIMEOUT)
+        configPeakCurrentLimit(peakCurrentLimit.Ampere.toInt(), TIMEOUT)
+        configPeakCurrentDuration(peakCurrentDuration.milli(Second).toInt(), TIMEOUT)
+        configSelectedFeedbackSensor(FeedbackDevice.Analog, IDX, TIMEOUT)
+        configVelocityMeasurementPeriod(Period_5Ms, TIMEOUT)
+        configVelocityMeasurementWindow(4, TIMEOUT)
+
+        val configs = TalonSRXConfiguration().also { getAllConfigs(it, TIMEOUT) }
+        check(configs.reverseSoftLimitThreshold == min.second && configs.forwardSoftLimitThreshold == max.second) {
+            "Soft limits are not set correctly"
+        }
+    }
+
+    val position: Length
+        get() {
+            val nativeFeedbackUnits = max.second - min.second
+            val perFeedbackQuantity = max.first - min.first
+
+            return (perFeedbackQuantity / nativeFeedbackUnits * talon.getSelectedSensorPosition(IDX)) - zeroOffset
+        }
+
+    fun set(value: DutyCycle) {
+        talon.set(ControlMode.PercentOutput, value.Each)
+    }
+
+}
