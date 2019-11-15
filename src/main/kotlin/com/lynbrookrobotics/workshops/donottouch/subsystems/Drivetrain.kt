2b20a6581f5ec6aff182ce704b9c557707496ad5
Andy Min <andrewkmin@gmail.com>
2019-11-15 13:24:56 -0800

    Feedback

--- src/main/kotlin/com/lynbrookrobotics/workshops/donottouch/subsystems/Drivetrain.kt
+++ src/main/kotlin/com/lynbrookrobotics/workshops/donottouch/subsystems/Drivetrain.kt
@@ -1,15 +1,19 @@
 package com.lynbrookrobotics.workshops.donottouch.subsystems
 
 import com.ctre.phoenix.motorcontrol.ControlMode
-import info.kunalsheth.units.generated.DutyCycle
-import info.kunalsheth.units.generated.Each
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import info.kunalsheth.units.generated.*
+import info.kunalsheth.units.math.milli
 
 class Drivetrain {
 
     private val hardware = DrivetrainHardware()
 
+    val feedback get() = hardware.talon.getSelectedSensorVelocity(0).Each / 100.milli(Second)
+
     fun output(value: DutyCycle) {
-        hardware.talon.set(ControlMode.PercentOutput, value.Each)
+        SmartDashboard.putNumber("Drivetrain Feedback", feedback.Hertz)
+        hardware.talon.set(ControlMode.PercentOutput, -value.Each)
     }
 
 }
